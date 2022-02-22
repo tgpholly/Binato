aef1f68cc8c9b24da9c19a74c771294827d43a29
Holly <onlinegamer748@gmail.com>
2022-02-22 10:23:25 +0000

    capitalise the "B" in Bancho

--- Binato.js
+++ Binato.js
@@ -66,7 +66,7 @@ app.use((req, res) => {
 				if (req.headers["host"].split(".")[0][0] == "c")
 					serverHandler(req, res);
 				else
-					res.status(400).send("400 | Bad Request!<br>Binato only accepts post requests on bancho subdomains.<hr>Binato");
+					res.status(400).send("400 | Bad Request!<br>Binato only accepts post requests on Bancho subdomains.<hr>Binato");
 			break;
 
 			default:
