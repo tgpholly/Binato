b40361caf0ad9e07c45e5e1b53909a69c458b7db
Holly <onlinegamer748@gmail.com>
2023-10-16 10:52:36 +0100

    Replace fileSmasher with ncc.

--- tooling/mangle.ts
+++ tooling/mangle.ts
@@ -5,11 +5,11 @@ const DISABLE = false;
 writeFileSync("./build/.MANGLED", `${DISABLE}`);
 
 if (DISABLE) {
-	writeFileSync("./build/Binato.js", readFileSync("./build/combined.js"));
+	writeFileSync("./build/Binato.js", readFileSync("./build/index.js"));
 	console.warn("[WARNING] mangle.ts is disabled!");
 } else {
 	(async () => {
-		const mangled = await minify(readFileSync("./build/combined.js").toString(), {
+		const mangled = await minify(readFileSync("./build/index.js").toString(), {
 			mangle: true,
 			toplevel: true,
 		});
